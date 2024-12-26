fbfa476a81241d14ce2faefeee81a11da5dc57df
Enrique Madrid <emadridm@gmail.com>
2024-12-26 10:06:28 -0500

    backup

---
+++ test/commands/create.test.ts
@@ -0,0 +1,19 @@
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
+
+describe('create', () => {
+    it('runs create cmd', async () => {
+        const {stdout} = await runCommand('create')
+        expect(stdout).to.contain('hello world')
+    })
+
+    it('runs create --name oclif', async () => {
+        const {stdout} = await runCommand('create --name oclif')
+        expect(stdout).to.contain('hello oclif')
+    })
+
+    it('runs create mySchema', async () => {
+        const {stdout} = await runCommand('create mySchema')
+        expect(stdout).to.contain('mySchema')
+    })
+})
