cbd075ce0e8becdedb014ffcb8ddeb12319f54a1
Enrique Madrid <emadridm@gmail.com>
2024-12-27 13:54:11 -0500

    create a minimalist JSON Schema done

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,30 +1,20 @@
+import {Schema} from '@iterativa/core/schema'
 import {Args, Command, Flags} from '@oclif/core'
 
 export default class Create extends Command {
-  static override args = {
-    file: Args.string({description: 'file to read'}),
-  }
-
-  static override description = 'describe the command here'
-
-  static override examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
-
-  static override flags = {
-    // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
-  }
-
-  public async run(): Promise<void> {
-    const {args, flags} = await this.parse(Create)
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /home/kiko-madrid/Projects/iterativa-schema/src/commands/create.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    static override args = {
+        name: Args.string({description: 'name of the schema', required: true}),
     }
-  }
+
+    static override description = 'create a JSON Schema'
+
+    static override examples = ['<%= config.bin %> <%= command.id %>']
+
+    public async run(): Promise<void> {
+        const {args} = await this.parse(Create)
+        const schema = new Schema()
+
+        console.log(args.name)
+        console.log(schema.toJSONSchema())
+    }
 }
--- test/commands/create.test.ts
+++ test/commands/create.test.ts
@@ -2,18 +2,9 @@ import {runCommand} from '@oclif/test'
 import {expect} from 'chai'
 
 describe('create', () => {
-    it('runs create cmd', async () => {
-        const {stdout} = await runCommand('create')
-        expect(stdout).to.contain('hello world')
-    })
-
-    it('runs create --name oclif', async () => {
-        const {stdout} = await runCommand('create --name oclif')
-        expect(stdout).to.contain('hello oclif')
-    })
-
     it('runs create mySchema', async () => {
         const {stdout} = await runCommand('create mySchema')
         expect(stdout).to.contain('mySchema')
+        expect(stdout).to.contain('https://json-schema.org/draft/2020-12')
     })
 })
